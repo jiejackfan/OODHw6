55b3ca4a8bdf091b9161aab7690568677c9003a5
yajingwang1022 <wang.yaj@husky.neu.edu>
2020-03-21 14:40:53 -0400

    Added TextualView and VisualView as interfaces.

--- src/cs3500/animator/view/IView.java
+++ src/cs3500/animator/view/IView.java
@@ -5,13 +5,5 @@ package cs3500.animator.view;
  */
 public interface IView {
 
-  /**
-   * Refresh view to reflect change in the game state.
-   */
-  void refresh();
 
-  /**
-   * Make the view visible when starting the animation.
-   */
-  void makeVisible();
 }
