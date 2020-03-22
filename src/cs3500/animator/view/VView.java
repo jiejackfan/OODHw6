f831196796e74982fce4996819390e98d068a15e
Jack <fan.jie1@husky.neu.edu>
2020-03-21 21:42:44 -0400

    added view classes

---
+++ src/cs3500/animator/view/VView.java
@@ -0,0 +1,16 @@
+package cs3500.animator.view;
+
+import javax.swing.JFrame;
+
+public class VView extends JFrame implements VisualView {
+
+  @Override
+  public void refresh() {
+
+  }
+
+  @Override
+  public void makeVisible() {
+
+  }
+}
